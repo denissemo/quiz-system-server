2bdfdf83df871846708c68f774adad53e783de86
denissemo <denissemo1999@gmail.com>
2020-06-14 23:52:07 +0300

    remove test

--- test.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const config = require('./config');
-
-module.exports = {
-  'config': config.dbConfig,
-  'models-path': path.resolve('database', 'model'),
-  'migrations-path': path.resolve('database', 'migrations')
-}
